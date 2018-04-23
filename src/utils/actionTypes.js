2bbb0ce4883f9dafe852b59d93a20af21229b064
luc <550502661@qq.com>
2018-04-24 00:00:11 +0800

    ActionTypes

--- src/utils/actionTypes.js
+++ src/utils/actionTypes.js
@@ -3,6 +3,13 @@
  * For any unknown actions, you must return the current state.
  * If the current state is undefined, you must return the initial state.
  * Do not reference these action types directly in your code.
+ * 
+ * 这些是Redux预留的私人行为类型。
+ * 对于任何未知的动作，必须返回当前状态。
+ * 如果当前状态未定义，则必须返回初始状态。
+ * 不要在代码中直接引用这些操作类型。
+ * 
+ * redux自己创建的action 用来初始化状态树 和 reducer 改变后初始化状态树
  */
 const ActionTypes = {
   INIT:
