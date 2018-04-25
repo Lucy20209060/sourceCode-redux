171321184c48e577bcd9cc856edb6fbf6c9e1ecf
luc <550502661@qq.com>
2018-04-25 21:49:56 +0800

    compose

--- src/compose.js
+++ src/compose.js
@@ -24,3 +24,21 @@ export default function compose(...funcs) {
 非常简洁的代码 最后一句 非常函数式的一句代码
 先看reduce方法
 reduce接受两个参数 第一个参数是一个callback函数 第二个是一个初始值 initValue
+
+第一个函数有四个参数
+
+previousValue：上一次调用callback时返回的值
+currentValue:正在处理的数组元素
+index：正在处理的数组元素下标
+array:被处理的数组
+
+如果有initValue，initValue将作为第一次的previousValue
+若没有 则数组第一个元素将作为previousValue 后面一个元素将作为currentValue
+然后执行callback的函数体 将返回的值作为previousValue
+将下一个元素作为currentValue 一直到最后一个数组最后一个元素执行完位置 再返回最终的结果
+
+比如 arr = [1,2,3,4,5] 使用reduce求和
+let sun = [1,2,3,4,5].reduce((a,b) =>a+b))
+
+compose巧妙的在于每个元素都是函数 callback返回一个复合函数作为previousValue
+在reduce 方法执行后 也就返回了一个将整个数组中所有函数串式调用的一个函数 
