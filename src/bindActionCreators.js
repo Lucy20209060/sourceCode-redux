f4588529f2c03ba31233391598999f469c1b598b
luc <550502661@qq.com>
2018-05-02 22:14:00 +0800

    bindActionCreators

--- src/bindActionCreators.js
+++ src/bindActionCreators.js
@@ -25,11 +25,15 @@ function bindActionCreator(actionCreator, dispatch) {
  * function as `actionCreators`, the return value will also be a single
  * function.
  */
+// 将action与dispatch函数绑定 生成直接可以触发action的函数
+// 可以将第一个参数对象中所有的action都直接生成可以直接触发dispatch的函数
+// 而不需要一个一个的dispatch 生成后的方法对应原来action生成器的函数名
 export default function bindActionCreators(actionCreators, dispatch) {
   if (typeof actionCreators === 'function') {
     return bindActionCreator(actionCreators, dispatch)
   }
 
+  // actionCreators 必须为object类型
   if (typeof actionCreators !== 'object' || actionCreators === null) {
     throw new Error(
       `bindActionCreators expected an object or a function, instead received ${
@@ -44,9 +48,12 @@ export default function bindActionCreators(actionCreators, dispatch) {
   for (let i = 0; i < keys.length; i++) {
     const key = keys[i]
     const actionCreator = actionCreators[key]
+    // 给 actionCreator的每一个成员都绑定dispatch方法生成新的方法
+    // 然后注入新的对象中 新方法对应的key即为原来在 actionCreators 的名字
     if (typeof actionCreator === 'function') {
       boundActionCreators[key] = bindActionCreator(actionCreator, dispatch)
     }
   }
   return boundActionCreators
 }
+// 这个方法主要的作用就是将action与dispatch函数绑定，生成直接可以触发action的函数
